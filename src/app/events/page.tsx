1f63bc38c98c2796d32e1b5ad7be19c29ccd83c8
katiechicot17-hash <katiechicot17@gmail.com>
2025-09-05 08:43:02 +0100

    Update page.tsx

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -7,6 +7,6 @@ export default function Events() {
     <style global jsx>{`body{--bg-gradient: var(--p4);}`}</style>
     <Image src={'/images/banner-3.jpg'} className={'hero'} alt={''} width={820} height={200}/>
     <h1>Upcoming Events</h1>
-    <p>Coming soon…</p>
+    <p>Coming soon…the launch of MathsWeek England</p>
   </div>);
 }
