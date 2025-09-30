4503a800d6db5f9d890442dae7ff5ee842c6db7f
Philipp Legner <plegner@amplify.com>
2025-09-30 18:55:08 +0100

    Updated codebreaking results

--- src/app/codebreaking/page.tsx
+++ src/app/codebreaking/page.tsx
@@ -1,18 +1,80 @@
 "use client";
 import Image from 'next/image'
+import {useState} from 'react'
+import styles from './page.module.css';
+
+const ANIMALS = [
+  'badger',
+  'bald-eagle',
+  'bear',
+  'butterfly',
+  'camel',
+  'cat',
+  'cockerel',
+  'cow',
+  'crab',
+  'dog',
+  'duck',
+  'elephant',
+  'kingfisher',
+  'koala',
+  'lion',
+  'meerkat',
+  'monkey',
+  'octopus',
+  'owl',
+  'panda',
+  'penguin',
+  'pig',
+  'pocupine',
+  'rabbit',
+  'red-fox',
+  'rhinoceros',
+  'shark',
+  'sloth',
+  'snake',
+  'spider',
+  'squirrel',
+  'turtle',
+  'whale',
+  'zebra',
+];
 
 export default function Events() {
+  const [checkedAnimals, setCheckedAnimals] = useState<Set<string>>(new Set());
+
+  const toggleAnimal = (animal: string) => {
+    setCheckedAnimals(prev => {
+      const newSet = new Set(prev);
+      if (newSet.has(animal)) {
+        newSet.delete(animal);
+      } else {
+        newSet.add(animal);
+      }
+      return newSet;
+    });
+  };
+
   return (<div>
     <title>MathsWorld London – Codebreaking Solution</title>
-    <style global jsx>{`.row img { mix-blend-mode: multiply; }`}</style>
-    <h1>Codebreaking Solutions</h1>
+    <h1>Codebreaking</h1>
     <h2 style={{textAlign: 'center'}}>Did you find all these animals?</h2>
-    <div className={'row'}>
-      <Image src={'/images/codebreaking/bear.png'} alt={'Bear'} width={250} height={250}/>
-      <Image src={'/images/codebreaking/camel.png'} alt={'Camel'} width={250} height={250}/>
-      <Image src={'/images/codebreaking/badger.png'} alt={'Badger'} width={250} height={250}/>
-      <Image src={'/images/codebreaking/bald-eagle.png'} alt={'Eagle'} width={250} height={250}/>
-      <Image src={'/images/codebreaking/butterfly.png'} alt={'Butterfly'} width={250} height={250}/>
+    <div className="row" style={{gap: '1rem'}}>
+      {ANIMALS.map(animal => (
+        <div
+          key={animal}
+          className={`${styles.animal} ${checkedAnimals.has(animal) ? styles.checked : ''}`}
+          onClick={() => toggleAnimal(animal)}
+        >
+          <Image
+            src={`/images/codebreaking/${animal}.png`}
+            alt={animal}
+            width={150}
+            height={150}
+          />
+          <div className={styles.checkbox}></div>
+        </div>
+      ))}
     </div>
   </div>);
 }
